2bbd7d57af2049fa4991ad33ad9ded31abcf8268
TimWaskett <tim.waskett@gmail.com>
2016-03-13 19:29:15 +0000

    Added colliders and rigidbody to telepresence bot so it can push things.

--- assets/Scripts/Test/TelepresenceBotTestControl.cs
+++ assets/Scripts/Test/TelepresenceBotTestControl.cs
@@ -1,15 +1,32 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(Rigidbody))]
 public class TelepresenceBotTestControl : MonoBehaviour
 {
     [SerializeField] float m_forwardSpeed = 1f;
     [SerializeField] float m_rotationSpeed = 90f;
 
+    private Rigidbody m_rigidbody;
+
+
+    void Awake()
+    {
+        m_rigidbody = GetComponent<Rigidbody>();
+    }
+
 
 	void Update()
-    {
-        transform.Translate(Vector3.forward * Time.deltaTime * m_forwardSpeed * Input.GetAxis("Vertical"));
+    { 
         transform.Rotate(Vector3.up * Time.deltaTime * m_rotationSpeed * Input.GetAxis("Horizontal"));
+    }
+
+
+    void FixedUpdate()
+    {
+        float verticalVelocity = m_rigidbody.velocity.y;
+        float forwardVelocity = m_forwardSpeed * Input.GetAxis("Vertical");
+        var newVelocity = transform.forward * forwardVelocity + Vector3.up * verticalVelocity;
+        m_rigidbody.velocity = newVelocity;
     }
 }
