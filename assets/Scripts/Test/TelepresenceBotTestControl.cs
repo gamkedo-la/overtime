cc0b00d1a0a39f609c9b508ed1d0039b543ff2ca
TimWaskett <tim.waskett@gmail.com>
2016-03-13 15:05:16 +0000

    Updated the telepresence bot model and added a test control script

---
+++ assets/Scripts/Test/TelepresenceBotTestControl.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TelepresenceBotTestControl : MonoBehaviour
+{
+    [SerializeField] float m_forwardSpeed = 1f;
+    [SerializeField] float m_rotationSpeed = 90f;
+
+
+	void Update()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * m_forwardSpeed * Input.GetAxis("Vertical"));
+        transform.Rotate(Vector3.up * Time.deltaTime * m_rotationSpeed * Input.GetAxis("Horizontal"));
+    }
+}
