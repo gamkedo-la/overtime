bb6fe2d068a8481e32af76a75db75cc51ef72159
TimWaskett <tim.waskett@gmail.com>
2016-03-14 19:36:05 +0000

    Updated the physics of the TB so it feels more realistic.

--- assets/Scripts/Test/TelepresenceBotTestControl.cs
+++ assets/Scripts/Test/TelepresenceBotTestControl.cs
@@ -2,31 +2,58 @@
 using System.Collections;
 
 [RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Animator))]
 public class TelepresenceBotTestControl : MonoBehaviour
 {
-    [SerializeField] float m_forwardSpeed = 1f;
+    [SerializeField] float m_forwardForce = 10f;
+    [SerializeField] float m_maxForwardSpeed = 2f;
     [SerializeField] float m_rotationSpeed = 90f;
 
     private Rigidbody m_rigidbody;
-
+    private Animator m_anim;
+    private bool m_movingBefore;
+    private float m_forwardVelocity;
 
     void Awake()
     {
         m_rigidbody = GetComponent<Rigidbody>();
+        m_anim = GetComponent<Animator>();
     }
 
 
 	void Update()
     { 
         transform.Rotate(Vector3.up * Time.deltaTime * m_rotationSpeed * Input.GetAxis("Horizontal"));
+
+        bool moving = Mathf.Abs(m_forwardVelocity) > 0.01f;
+
+        if (moving != m_movingBefore)
+        {
+            m_movingBefore = moving;
+
+            if (moving)
+                m_anim.SetTrigger("Up");
+            else
+                m_anim.SetTrigger("Down");
+        }
     }
 
 
     void FixedUpdate()
     {
+        float input = Input.GetAxisRaw("Vertical");
+
+        m_rigidbody.AddForce(transform.forward * m_forwardForce * input);
+
         float verticalVelocity = m_rigidbody.velocity.y;
-        float forwardVelocity = m_forwardSpeed * Input.GetAxis("Vertical");
-        var newVelocity = transform.forward * forwardVelocity + Vector3.up * verticalVelocity;
-        m_rigidbody.velocity = newVelocity;
+        m_forwardVelocity = transform.InverseTransformDirection(m_rigidbody.velocity).z;
+
+        if (Mathf.Abs(m_forwardVelocity) > m_maxForwardSpeed)
+            m_forwardVelocity = Mathf.Sign(m_forwardVelocity) * m_maxForwardSpeed;
+
+        print(m_forwardVelocity);
+
+        var newVelocity = transform.forward * m_forwardVelocity + Vector3.up * verticalVelocity;
+        m_rigidbody.velocity = newVelocity;        
     }
 }
