38b36fec07b66116a904ee9ec31272d15315c3be
TimWaskett <tim.waskett@gmail.com>
2016-04-03 14:29:06 +0100

    Added improved TB patrol AI.

--- assets/Scripts/Telepresence bot/TelepresenceBotAiPatrol.cs
+++ assets/Scripts/Telepresence bot/TelepresenceBotAiPatrol.cs
@@ -8,6 +8,8 @@ public class TelepresenceBotAiPatrol : MonoBehaviour
     [SerializeField] float m_rotationSpeed = 180f;
     [SerializeField] float m_decisionRate = 0.1f;
     [SerializeField] LayerMask m_avoidanceLayerMask;
+    [SerializeField] float m_rayCastHeight = 0.5f;
+    [SerializeField] float m_rayCastRange = 1.5f;
 
     private TelepresenceBotMotor m_motor;
 
@@ -39,17 +41,89 @@ public class TelepresenceBotAiPatrol : MonoBehaviour
         float vertical = m_moving ? 1f : 0f;
         float horizontal = m_turning ? m_turnDirection : 0f;
 
-        m_motor.Move(vertical, horizontal);
-
-        
+        m_motor.Move(vertical, horizontal);   
     }
 
 
     private IEnumerator CheckSurroundings()
     {
+        yield return new WaitForSeconds(Random.Range(0f, m_decisionRate));
+
         while (true)
         {
-            m_turnDirection = (int) Mathf.Sign(Random.Range(-1f, 1f));
+            RaycastHit hitForward;
+            RaycastHit hitForwardLeft;
+            RaycastHit hitForwardRight;
+
+            var origin = transform.position + Vector3.up * m_rayCastHeight;
+
+            var rayDirectionForward = transform.forward * m_rayCastRange;
+            var rayDirectionForwardLeft = (transform.forward - transform.right).normalized * m_rayCastRange;
+            var rayDirectionForwardRight = (transform.forward + transform.right).normalized * m_rayCastRange;
+
+            var rayForward = new Ray(origin, rayDirectionForward);
+            var rayForwardLeft = new Ray(origin, rayDirectionForwardLeft);
+            var rayForwardRight = new Ray(origin, rayDirectionForwardRight);
+
+            bool impactForward;
+            bool impactForwardLeft;
+            bool impactForwardRight;
+
+            if (Physics.Raycast(rayForward, out hitForward, m_rayCastRange, m_avoidanceLayerMask))
+            {
+                impactForward = true;
+                Debug.DrawLine(origin, origin + rayDirectionForward, Color.red);
+            }
+            else
+            {
+                impactForward = false;
+                Debug.DrawLine(origin, origin + rayDirectionForward, Color.green);
+            }
+
+            if (Physics.Raycast(rayForwardLeft, out hitForwardLeft, m_rayCastRange, m_avoidanceLayerMask))
+            {
+                impactForwardLeft = true;
+                Debug.DrawLine(origin, origin + rayDirectionForwardLeft, Color.red);
+            }
+            else
+            {
+                impactForwardLeft = false;
+                Debug.DrawLine(origin, origin + rayDirectionForwardLeft, Color.green);
+            }
+
+            if (Physics.Raycast(rayForwardRight, out hitForwardRight, m_rayCastRange, m_avoidanceLayerMask))
+            {
+                impactForwardRight = true;
+                Debug.DrawLine(origin, origin + rayDirectionForwardRight, Color.red);
+            }
+            else
+            {
+                impactForwardRight = false;
+                Debug.DrawLine(origin, origin + rayDirectionForwardRight, Color.green);
+            }
+
+            if (impactForwardLeft && !impactForwardRight)
+            {
+                m_turnDirection = 1;
+                m_turning = true;
+            }
+            else if (impactForwardRight && !impactForwardLeft)
+            {
+                m_turnDirection = -1;
+                m_turning = true;
+            }
+            else if (impactForward || (impactForwardLeft && impactForwardRight))
+            {
+                if (!m_turning)
+                    m_turnDirection = (int) Mathf.Sign(Random.Range(-1f, 1f));
+
+                m_turning = true;
+            }
+            else
+            {
+                m_turnDirection = 0;
+                m_turning = false;
+            }
 
             yield return new WaitForSeconds(m_decisionRate);
         }
