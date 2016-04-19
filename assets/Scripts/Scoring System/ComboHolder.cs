f4d844eab02d91210ba6eb56fc614fb3f43ead6f
MaesterLazer <grfxcoordinator@GRFX_313_4.aspectratio.lan>
2016-04-18 20:17:30 -0700

    Combo Progress

---
+++ assets/Scripts/Scoring System/ComboHolder.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ComboHolder : MonoBehaviour {
+
+	public static ComboHolder instance;
+	public static List<string> activeComboList = new List<string>();
+	
+
+
+	// Use this for initialization
+	void Start () {
+		instance = this;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
