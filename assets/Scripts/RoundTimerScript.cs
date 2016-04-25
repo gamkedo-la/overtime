127e9d9ec26a4a19bf5405e952f03c5d76ebc6a1
MaesterLazer <grfxcoordinator@GRFX_313_4.aspectratio.lan>
2016-04-25 14:30:01 -0700

    Created GameManager GO, Round Timer Script, Round Timer UI, Environment made into Prefab for revising in LevelEditor Scene

---
+++ assets/Scripts/RoundTimerScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class RoundTimerScript : MonoBehaviour {
+
+	public float roundTimerInMinutes = 5f;
+	private float roundTimerInSeconds;
+	public bool timeStarted = false;
+	[SerializeField] string minutes;
+	[SerializeField] string seconds;
+	public GameObject timeCount;
+	private Text text;
+
+	void Start () {
+	
+		text = timeCount.transform.GetComponent<Text> ();
+		roundTimerInSeconds = (roundTimerInMinutes * 60F);
+	}
+
+
+	void Update()
+	{
+		if(timeStarted == true)
+		{
+			roundTimerInSeconds -= Time.deltaTime;
+		}
+		minutes = (Mathf.Floor(roundTimerInSeconds / 60).ToString("00"));
+		seconds = (Mathf.Floor(roundTimerInSeconds % 60).ToString("00"));
+
+		text.text = (minutes + ":" + seconds);
+	}
+}
+
+	
