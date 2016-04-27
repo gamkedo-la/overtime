9758f1703e88799758f2372b36bd1684cc281334
MaesterLazer <grfxcoordinator@GRFX_313_4.aspectratio.lan>
2016-04-27 14:43:25 -0700

    Implemented code from PUN to set timer based off server

---
+++ assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager instance;
+
+	// Use this for initialization
+	void Start ()
+	{
+		instance = this;	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+
+	public void RoundOver ()
+	{
+
+	}
+}
