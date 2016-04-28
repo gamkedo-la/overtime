086524f95140014d4abe2a75aca99158420c2880
MaesterLazer <daniel.l.lazar@gmail.com>
2016-04-28 10:07:27 -0700

    Data carryover, need to fix relinks
    Round timer needs to not re-call RoundOver()
    RoundTimer needs to relink to Text component
    Resolve Photon ID issue
    Above may resolve the ln 56 error in GameManager

--- assets/Scripts/GameManager.cs
+++ assets/Scripts/GameManager.cs
@@ -6,14 +6,19 @@ public class GameManager : MonoBehaviour {
 	public static GameManager instance;
 	public bool postRound = false;
 	public PlayerScoreList playerScoreList;
+	public ScoreManager scoreManager;
+	public NetworkManagerScript networkManager;
+	public PlayerStats playerStats;
 
 	// Use this for initialization
 	void Start ()
 	{
 		instance = this;
+		DontDestroyOnLoad(transform.gameObject);
+
 		if (postRound)
 		{
-			playerScoreList.ForceScoreboardUpdate();
+
 		}
 	}
 	
@@ -27,6 +32,10 @@ public class GameManager : MonoBehaviour {
 	{
 		if(!postRound) // Load the Post-Round Stats Scene
 		{
+			// THINGS TO KEEP ACROSS THE LOAD //
+			DontDestroyOnLoad(networkManager.transform.gameObject);
+			instance.networkManager.enabled = false;
+
 			Application.LoadLevel("Main_PostRound");
 		}
 		else // This is the Post-Round, so restart the game
@@ -35,4 +44,22 @@ public class GameManager : MonoBehaviour {
 		}
 
 	}
+
+	void OnLevelWasLoaded(int level) { 
+		if (level == 1) // Set to the index of Post Round in Build Settings, UPDATE if changed
+		{ 
+			// Get the Network Manager after load
+			GameObject netMan = GameObject.Find("NetworkManager");
+			// Relink all the needed variables
+			instance.playerScoreList = netMan.transform.GetComponent<PlayerScoreList>();
+			instance.scoreManager = netMan.transform.GetComponent<ScoreManager>();
+			instance.playerScoreList.scoreManager = instance.scoreManager;
+
+			// Run the score update
+			playerScoreList.ForceScoreboardUpdate();
+
+		}	
+		
+	}
+
 }
