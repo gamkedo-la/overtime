4b7297974664fd696ac4219f70702c455226db7c
Chris DeLeon <gamkedo@gmail.com>
2016-05-19 10:23:02 -0700

    Second draft of over-the-shoulder camera, clips walls less, now rises/softly falls if obstructed to minimize clipping with character model either (has added utility in cubicle section of giving a way to 'peer over the top' and makes it at least a little less jumpy to navigate those areas)

--- assets/Scripts/FollowTargetV2.cs
+++ assets/Scripts/FollowTargetV2.cs
@@ -9,27 +9,44 @@ namespace UnityStandardAssets.Utility
         public Transform target;
         public Vector3 posOffset = new Vector3(0f, 0f, 0f);
        
-
+		private Vector3 lookFocus;
 
         private void LateUpdate()
         {
 			RaycastHit rhInfo;
 			int LayerFilter = ~LayerMask.GetMask ("AmmoBox","Player");
-			if (Physics.Raycast (target.position+Vector3.up,
-			                   target.transform.TransformVector(posOffset).normalized,
+			float raycastCamDist = 1.2f;
+			Vector3 camGoal;
+			Vector3 startPt = target.position+Vector3.up;
+			Vector3 endPt = target.transform.TransformVector(posOffset).normalized * raycastCamDist;
+			if (Physics.Raycast (startPt,
+			                   endPt,
 			                     out rhInfo,
-			                     1.2f,
+			                     raycastCamDist,
 			                   LayerFilter)) {
-				transform.position = rhInfo.point + rhInfo.normal*0.5f;
+				float goalDist = Vector3.Distance(startPt, endPt);
+				camGoal = rhInfo.point + rhInfo.normal*0.3f +
+							Vector3.up * (raycastCamDist - 
+					              Vector3.Distance(rhInfo.point,startPt)) * 1.2f;
 				Debug.Log (rhInfo.collider.name);
 			} else {
-				transform.position = target.position+Vector3.up + 
-					target.transform.TransformVector(posOffset).normalized * 1.2f;
+				camGoal = target.position+Vector3.up + 
+					target.transform.TransformVector(posOffset).normalized * raycastCamDist;
 			}
 
-			transform.rotation = Quaternion.Slerp(transform.rotation,
+			// sink back slower than it rises
+			float kVal = (camGoal.y < transform.position.y ? 0.98f : 0.75f);
+			transform.position = kVal * transform.position +
+				(1.0f - kVal) * camGoal;
+					
+			/*transform.rotation = Quaternion.Slerp(transform.rotation,
 			                                      target.rotation,
-			                                      Time.deltaTime*3.5f);
+			                                      Time.deltaTime*3.5f);*/
+			float lookTargetDist = (camGoal.y < transform.position.y ? 5.0f : 6.0f);
+			float lookKVal = 0.35f;
+			lookFocus = lookKVal * lookFocus +
+				(1.0f-lookKVal) * (target.position + target.forward * lookTargetDist);
+			transform.LookAt ( lookFocus );
         }
     }
 
